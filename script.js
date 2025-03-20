0abcea0a19e6b24541f19a06fb2911075ac2cebe
Abderahim <ita27rmp100@gmail.com>
2025-03-20 12:03:38 +0100

    new update : install packages I need made some changes

--- script.js
+++ script.js
@@ -3,27 +3,24 @@ let fsModule = require('fs')
 let qs = require('querystring')
 let requesIP = require('request-ip')
 let port = process.env.PORT || 2700
-let htmlCode = fsModule.readFileSync('index.html')
 
 let myHTTP = http.createServer(
     function(req,res){
-        let clientIP = requesIP.getClientIp(req)
+        let ipaddr = requesIP.getClientIp(req)
         let date = new Date()
-        res.end(htmlCode)
-        if (req.method=='POST'){
-            let body = ''
-            req.on('data',function(data){
-                body = body + data
-            })
-            req.on('end',function(){
-                let post = qs.parse(body)
-                // console.log(`Hello ${post.name} , your IP Adress is ${clientIP} .`)
-                fsModule.appendFile('dataUsers.txt',`New use from ${post.name} at ${date} . His/her IP Adress is : '${clientIP}' \n`, function(err) {
-                    if (err) throw err;
-                    console.log('Text appended to file\n');
-                  });
-            })
+        date = date.toGMTString()
+        const len = date.length + ipaddr.length
+        let _ = ""
+        for(i=0;i<=len+2;i++){
+            _ += "-"
         }
-    }
+
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.end(` <strong>Your IP Address is : </strong> ${ipaddr}`)
+            fsModule.appendFile('dataUsers.txt',`|${date}|${ipaddr}| \n${_}\n`, function(err) {
+                if (err) throw err;
+                console.log('Text appended to file\n');
+            });
+        }
 )
 myHTTP.listen(port)
